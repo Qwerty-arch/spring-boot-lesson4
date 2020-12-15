4766e27938ef3abc473e8dbd2ae797bf9fd8f714
Qwerty <oshovsky.vlad@yandex.ru>
2020-12-15 18:01:59 +0800

    commit 1

---
+++ src/test/java/com/oshovskii/boot/SpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oshovskii.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
